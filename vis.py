35d9eefa37b752590c0b6fcc05fdec41124a63f0
swgillespie <sean.william.g@gmail.com>
2012-07-17 11:17:19 +0200

    Modification of vis.py to allow for creation of cloud_large.png output

--- vis.py
+++ vis.py
@@ -1,6 +1,6 @@
 from pytagcloud import create_tag_image, make_tags
-from yahoo_passwords import YAHOO_PASSWORDS
+from yahoo_passwords import YAHOO_PASSWORDS_SMALL
 
-tags = make_tags(YAHOO_PASSWORDS, maxsize=120)
+tags = make_tags(YAHOO_PASSWORDS_SMALL, maxsize=120)
 
 create_tag_image(tags, 'cloud_large.png', size=(900, 600), fontname='Lobster')
