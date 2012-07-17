757f62f6e92374eba12f68024e2e32c373996303
Reid Baker <hamiltonbaker@yahoo.com>
2012-07-17 17:42:04 +0200

    now black and looking good

--- vis.py
+++ vis.py
@@ -3,9 +3,11 @@ from yahoo_passwords import YAHOO_PASSWORDS_SMALL
 from billabong_paswords import BILLABONG_PASWORDS_SMALL
 from myspace_passwords import MYSPACE_PASSWORDS_SMALL
 
-yahoo_tags = make_tags(YAHOO_PASSWORDS_SMALL, maxsize=120)
-billabong_tags = make_tags(BILLABONG_PASWORDS_SMALL, maxsize=120)
-myspace_tags = make_tags(MYSPACE_PASSWORDS_SMALL, maxsize=120)
+BLACK = ((0,0,0),)
+
+yahoo_tags = make_tags(YAHOO_PASSWORDS_SMALL, maxsize=120, colors=BLACK)
+billabong_tags = make_tags(BILLABONG_PASWORDS_SMALL, maxsize=120, colors=BLACK)
+myspace_tags = make_tags(MYSPACE_PASSWORDS_SMALL, maxsize=120, colors=BLACK)
 
 create_tag_image(yahoo_tags, 'cloud_yahoo.png', size=(900, 600), fontname='Lobster')
 create_tag_image(billabong_tags, 'cloud_billabong.png', size=(900, 600), fontname='Lobster')
