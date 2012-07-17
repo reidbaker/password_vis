815012d6316b9c22e49c5b5dc7fe689f6bd04074
Reid Baker <hamiltonbaker@yahoo.com>
2012-07-17 17:05:39 +0200

    billibong imporved

--- vis.py
+++ vis.py
@@ -4,7 +4,7 @@ from billabong_paswords import BILLABONG_PASWORDS_SMALL
 from myspace_passwords import MYSPACE_PASSWORDS_SMALL
 
 yahoo_tags = make_tags(YAHOO_PASSWORDS_SMALL)
-billabong_tags = make_tags(BILLABONG_PASWORDS_SMALL)
+billabong_tags = make_tags(BILLABONG_PASWORDS_SMALL, maxsize=120)
 myspace_tags = make_tags(MYSPACE_PASSWORDS_SMALL, maxsize=120)
 
 create_tag_image(yahoo_tags, 'cloud_yahoo.png', size=(900, 600), fontname='Lobster')
